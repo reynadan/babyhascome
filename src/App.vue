2b73cddc0af427fe4b7595b0cef89baa1e41c5df
Daniel Reynaud <reynauddaniel26@gmail.com>
2025-07-20 19:11:53 +0000

    Add first pronostic page

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div id="app">
+    <PronosticsResults />
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+import PronosticsResults from './components/PronosticsResults.vue'
+</script>
