fd9072cb9e39a58f5d2342b2972e93e105ee2540
Daniel Reynaud <reynauddaniel26@gmail.com>
2025-07-25 15:09:35 +0000

    Change wording

--- src/components/Page5.vue
+++ src/components/Page5.vue
@@ -3,7 +3,7 @@
     class="min-h-screen flex flex-col justify-center items-center text-center"
   >
     <h2 class="text-2xl md:text-4xl mb-4">
-      Vous pensiez donc, en médiane, qu'il <u>arriverait</u> le
+      Vous pensiez donc, en médiane,<br/> que bébé <u>arriverait</u> le
     </h2>
     <p class="text-4xl md:text-6xl mb-6">{{ estimatedBirthDate }}</p>
     <p class="text-sm md:text-base opacity-0 fade-in">
