ade5471ef289d2ebaf83004ac59df94e6182827d
Daniel Reynaud <reynauddaniel26@gmail.com>
2025-09-17 10:05:56 +0000

    Optimise images

--- src/components/Page14.vue
+++ src/components/Page14.vue
@@ -4,7 +4,7 @@
   >
     <h1 class="text-2xl md:text-4xl text-center px-6">
       Notre petit guerrier est actuellement en néonatalogie pour une durée indeterminée pour soigner une infection pulmonaire.<br/>
-      Nous serons donc un peu absent pour les prochains jours afin d'être auprès de lui.<br/>
+      Nous serons donc un peu absents les prochains jours afin d'être auprès de lui.<br/>
       N'hésitez pas à nous demander des nouvelles, on vous répondra avec plaisir.
     </h1>
     <br />
